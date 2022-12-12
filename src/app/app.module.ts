87b933b554e16057db34423b827d756472897407
Elyes <elyess.laiche@gmail.com>
2022-12-12 15:54:11 +0100

    Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { UserComponent } from './user/user.component';
+import { OrdersComponent } from './orders/orders.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    UserComponent,
+    OrdersComponent,
+    ContactFormComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/contact-form/contact-form.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'contact-form',
+  templateUrl: './contact-form.component.html',
+  styleUrls: ['./contact-form.component.css']
+})
+
+export class ContactFormComponent {
+
+  Name: string = '';
+
+  validate():void{
+
+  }
+}
